98a8ad8d32ce0b82116371680dbd449452f37d1b
begimqulovstiverna-hub <begimqulovstiverna@gmail.com>
2026-02-21 13:09:57 +0500

    Update [id].tsx67

--- pages/groups/[id].tsx
+++ pages/groups/[id].tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { motion, AnimatePresence } from 'framer-motion'
 import Navbar from '../../components/Navbar'
 
+
 interface Group {
   id: string
   name: string
